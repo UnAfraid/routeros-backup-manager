4bd0512f598902d222589dee777883504c2a6602
UnAfraid <unafraid89@gmail.com>
2025-09-01 14:54:32 +0300

    Deliver notifications on exception

--- app/Filament/Resources/Device/Actions/CreateBackupAction.php
+++ app/Filament/Resources/Device/Actions/CreateBackupAction.php
@@ -13,7 +13,6 @@ class CreateBackupAction
         return Action::make($name)
             ->label('Run Backup')
             ->icon('heroicon-o-arrow-path')
-            ->requiresConfirmation()
             ->action(function ($record) {
                 $recipient = auth()->user();
 
@@ -21,14 +20,18 @@ class CreateBackupAction
                     $job = new CreateBackupJob($record);
                     $job->handle();
 
-                    Notification::make()
-                        ->title('Created backup successfully')
-                        ->sendToDatabase($recipient);
+                    $recipient->notify(
+                        Notification::make()
+                            ->title('Created backup successfully')
+                            ->toDatabase()
+                    );
                 } catch (\Exception $e) {
-                    Notification::make()
+                    $recipient->notify(Notification::make()
                         ->title('Failed to create backup')
                         ->body($e->getMessage())
-                        ->danger();
+                        ->danger()
+                        ->toDatabase()
+                    );
                 }
             });
     }
