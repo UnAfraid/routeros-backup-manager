88bcff5a6c2c7f66385397537fe2ba81901e6abe
UnAfraid <unafraid89@gmail.com>
2025-09-04 01:55:58 +0300

    Enable scheduler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,17 +9,17 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withSchedule(function (Schedule $schedule): void {
-//        Device::all()->each(function (Device $routerOS) use ($schedule) {
-//            $schedule
-//                ->job(new CreateBackupJob($routerOS))
-//                ->cron($routerOS->backup_cron_schedule)
-//                ->name("backup-{$routerOS->id}");
-//        });
+        Device::all()->each(function (Device $device) use ($schedule) {
+            $schedule
+                ->job(new CreateBackupJob($device))
+                ->cron($device->backup_cron_schedule)
+                ->name("backup-{$device->id}");
+        });
     })
     ->withMiddleware(function (Middleware $middleware): void {
         //
